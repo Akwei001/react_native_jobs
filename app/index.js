fe122a2ef019a0da57c871a07b053568b4207c09
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-03-22 12:10:22 +0000

    home screen complete

--- app/index.js
+++ app/index.js
@@ -14,7 +14,26 @@ const Home = () => {
   const router = useRouter();
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-      <Stack.Screen></Stack.Screen>
+      <Stack.Screen
+        options={{
+          headerStyle: { backGroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />
+          ),
+          headerTitle: '',
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
